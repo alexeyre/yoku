e1e722d5f4803fb9915ec6ed186c9e81965fe4d1
Alex Holder <alexeeyre@gmail.com>
2025-11-12 15:31:56 +0000

    create ios app

---
+++ ios/yoku/yokuApp.swift
@@ -0,0 +1,17 @@
+//
+//  yokuApp.swift
+//  yoku
+//
+//  Created by Alex Holder on 12/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct yokuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
